ccc4392bbea8c494fa52ed2af7ea5e48b531e679
Ufuk Aksel <uaksel@k1m10s01.42istanbul.com.tr>
2023-09-14 04:49:02 +0300

    C02 ex10'a kadar

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uaksel <uaksel@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/14 02:01:07 by uaksel            #+#    #+#             */
+/*   Updated: 2023/09/14 02:03:43 by uaksel           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	size -= 1;
+	while (src[i] && (i < size))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (i);
+}
